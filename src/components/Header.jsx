a138304b457a4ea3bedca07b5689ce7d2d56ce0f
vladadev <vladimir.business0@gmail.com>
2023-07-27 01:45:50 +0200

    test 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../css/header.css'
 const Header = props => {
   return (
     <header>
-      <h1>TO DO:</h1>
+      <h1>TOoooo DO:</h1>
       <ul className="task-counter">
         <li>Total tasks: 12</li>
         <li>Completed: 2</li>
