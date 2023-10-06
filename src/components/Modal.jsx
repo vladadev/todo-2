a32e1b6292b2b18a048fc31ea2161486ac05821c
vladadev <vladimir.business0@gmail.com>
2023-10-07 01:56:22 +0200

    removed Modal.jsx commented code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,19 +2,6 @@ import { React, useState } from 'react'
 import '../css/modal.css'
 
 const Modal = ({ title, onConfirm, children }) => {
-  // const [inputListName, setInputListName] = useState('')
-
-  // const handleInputChange = event => {
-  //   let inputValue = event.target.value
-
-  //   setInputListName(inputValue)
-
-  //   console.log(addNewList)
-  //   console.log(modalInfo)
-  // }
-
-  // console.log(onConfirm)
-
   return (
     <div className="modal">
       <label htmlFor="list-name">{title} </label>
